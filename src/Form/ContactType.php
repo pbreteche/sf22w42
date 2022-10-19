b6f89f0a8bf27d96c342f0dd429c6c3d1c8f5255
Pierre Bretéché <pbreteche@dawan.fr>
2022-10-19 12:25:11 +0200

    Attach validation directly in a form

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -6,13 +6,26 @@ use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', null, [
+                'constraints' => [
+                    new Email(['message' => 'validé directement depuis le formulaire']),
+                    new Callback(function ($value, ExecutionContextInterface $context) {
+                        if (1 == 1) {
+                            $context->buildViolation('dumb validation')
+                                ->addViolation();
+                        }
+                    }),
+                ],
+            ])
             ->add('groupNames', null, [
                 'delimiter' => "\n",
             ])
