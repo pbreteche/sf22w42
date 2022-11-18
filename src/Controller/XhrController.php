2540e2bd846155020a2b98d99d334445b723cad2
Pierre Bretéché <pbreteche@dawan.fr>
2022-11-18 17:05:51 +0100

    translatable object

--- src/Controller/XhrController.php
+++ src/Controller/XhrController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\Translation\t;
 
 #[Route('/xhr')]
 class XhrController extends AbstractController
@@ -12,7 +13,10 @@ class XhrController extends AbstractController
     #[Route('/hello', methods: 'GET')]
     public function helloWorld(): Response
     {
+        $message = t('demo.translatable_object', ['adjective' => 'magnifique']);
         usleep(500);
-        return $this->render('xhr/hello_world.html.twig');
+        return $this->render('xhr/hello_world.html.twig', [
+            'message' => $message,
+        ]);
     }
 }
