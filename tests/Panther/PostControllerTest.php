8609d9c23f83035097c876fa20f62b130506b2cc
Pierre Bretéché <pbreteche@dawan.fr>
2022-11-18 14:16:47 +0100

    first test with Panther

---
+++ tests/Panther/PostControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Tests\Panther;
+
+use Symfony\Component\Panther\PantherTestCase;
+
+class PostControllerTest extends PantherTestCase
+{
+    public function testShow()
+    {
+        $client = static::createPantherClient(['browser' => static::FIREFOX]);
+
+        $crawler = $client->request('GET', '/post/1');
+        $this->assertPageTitleContains('Premier article');
+    }
+}
