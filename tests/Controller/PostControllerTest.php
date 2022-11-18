9ff39bd3f4fac78b726bc0483d76dcc24f152c5a
Pierre Bretéché <pbreteche@dawan.fr>
2022-11-18 12:07:54 +0100

    test invalid input in form

--- tests/Controller/PostControllerTest.php
+++ tests/Controller/PostControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostControllerTest extends WebTestCase
 {
@@ -20,8 +21,16 @@ class PostControllerTest extends WebTestCase
 
         $buttonElt = $crawler->selectButton('Save');
         $form = $buttonElt->form();
-        $form['post[title]'] = 'Titre de test';
         $form['post[body]'] = 'Corps de test';
+
+        $crawler = $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $titleField = $crawler->filter('[name="post[title]"]');
+        $errorMsg = $titleField->nextAll()->text();
+        $this->assertEquals('This value should not be blank.', $errorMsg);
+
+        $form['post[title]'] = 'Titre de test';
 
         $client->submit($form);
 
